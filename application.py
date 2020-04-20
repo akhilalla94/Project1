2a4dc59690c979f1be0de7c0f4dd369efb4b98b7
akhilalla94 <akhilalla1994@msitprogram.net>
2020-04-21 04:21:17 +0530

    Task 2 - Allow authenticated user to home and logout

--- application.py
+++ application.py
@@ -18,7 +18,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
-
 with app.app_context():
     db.create_all()
 
