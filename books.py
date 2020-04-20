a5c75bfe0033a31883850d05168db6cc5bd97166
akhilalla94 <akhilalla1994@msitprogram.net>
2020-04-21 04:17:41 +0530

    Importing books.csv

--- books.py
+++ books.py
@@ -2,7 +2,6 @@ import os,csv
 from booksdb import *
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import scoped_session,sessionmaker
-
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
